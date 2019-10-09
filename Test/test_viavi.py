12b3671887151dec920b4b891f7030df04be26ea
jclee <doradorn82@gmail.com>
2019-10-09 15:31:13 -0700

    update vector

--- Test/test_viavi.py
+++ Test/test_viavi.py
@@ -6,31 +6,36 @@ os.environ['SF56G_EVB_PATH'] = evb_path
 sys.path.insert(0,evb_path)
 from LibSF56G import SF56G
 
+# user
+tx_channels = [0,1,2,3]
+rx_channels = [0,1,2,3]
+
 chip = SF56G()
-chip.SetConfig('ext_clk',1)
-chip.SetConfig('lane_en',1)
+chip.SetConfig('ext_clk',0)
+#chip.SetConfig('b_dbg_print',True)
 chip.build()
 
 # sequence
 if 0:
     chip.test_apb() 
 if 1:
-    if(chip.init_evb() < 0):
-        print("apb failed")
-        exit
-    chip.set_datarate(53.125)
+    if(chip.init_evb(tx_channels) >= 0):
+        print("apb passed")
+        chip.set_datarate(53.125)
     #chip.act_chan_TX('PRBS31', channel=0)
-    chip.act_chan_TX('REMOTE', channel=0)
-    chip.act_chan_RX('PRBS31',channel=0)
-    chip.set_tx_pre_post(tx_pre1=-0.05,tx_post1=-0.15,attenuation=1)
-    
-    chip.tune_init()
-    
-    #chip.tune_rx()
-    chip.mCs.GetHistogram()
-    chip.display_rx_eq(channel=0)
-    chip.mPmad.PrintLaneState()
-    #chip.GetHistogram()
-    ber= chip.get_status(measure_bits_db=36,HeightOnly=1,extra_ber_en=1,imp_eq_out=0)
-    print("ber=%6.2e" % (ber['ber']))
-    #print(ber)
+        for ch in tx_channels:
+            chip.act_chan_TX('REMOTE', channel=ch)
+        for ch in rx_channels:
+            chip.act_chan_RX('REMOTE',channel=ch)
+        for ch in tx_channels:
+            chip.set_tx_pre_post(tx_pre1=-0.05,tx_post1=-0.15,attenuation=1,channel=ch)
+        for ch in rx_channels:
+            chip.tune_init(channel=ch)
+        
+        for ch in range(4):
+            chip.mPmad.PrintLaneState(ch)
+        for ch in range(4):
+            chip.mPmad.PrintAllRxCoef(ch)
+            chip.GetHistogram(ch)
+    else:
+        print("apb failed")
